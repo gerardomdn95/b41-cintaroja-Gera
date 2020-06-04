c53b189e467a563bb80c6e8aa13dc00798e96aa6
Gerardo Medina <gerardo@devf.mx>
2020-06-03 20:02:05 -0500

    add main.js

--- 6_Promesas/Banda.js
+++ 6_Promesas/Banda.js
@@ -1,4 +1,5 @@
 class Banda {
+  // Método especial
   constructor(nombre, anio, website, genero) {
     this.nombre = nombre;
     this.anio = anio;
@@ -7,4 +8,32 @@ class Banda {
   }
 }
 
+// Exporto la clase como un objeto
 module.exports = { Banda };
+
+// console.log({ Banda });
+
+// ------------------------------------------------------------------------------------------------
+
+// Objeto Literal
+// const BandaX = {
+//   nombre: 'Nirvana',
+//   anio: 1990,
+//   website: 'www.nirvana.com',
+//   genero: 'Rock'
+// }
+
+// console.log(BandaX);
+
+// const nombre = BandaX.nombre;
+// const anio = BandaX.anio;
+// const website = BandaX.website;
+// const genero = BandaX.genero;
+
+// Destructuración de Objetos
+// const { nombre, anio, website, genero } = BandaX;
+
+// console.log(website);
+// console.log(anio);
+// console.log(nombre);
+// console.log(genero);
--- 6_Promesas/main.js
+++ 6_Promesas/main.js
@@ -1,24 +1,37 @@
+// Llamamos a nuestra dependencia que se llama request
+// request nos va a permitir hacer peticiones http
 const request = require('request');
+
+// Destructuración de mi objeto que se exportó de Banda.js
 const { Banda } = require('./Banda');
 
+// Declarando mi función, buscaBanda y recibe un parámetro o argumento (nombre)
 const buscaBanda = (nombre) => {
   // Retorna una promesa
   return new Promise((resolve, reject) => {
-    // URL
+    // URL => creamos nuestra constante endPoint y concatenamos el valor nombre
     const endPoint = `http://www.theaudiodb.com/api/v1/json/1/search.php?s=${nombre}`;
     // Ejecutamos request
+    // request recibe como primer parámetro endPoint
+    // callback function (error, response, body)
+    // error => me va a indicar si existe un error
+    // response => información de la petición (status code)
+    // body => JSON que recibimos
     request(endPoint, (error, response, body) => {
+      // Si no hay errores, continuamos
       if (error === null) {
-        // console.log('Encontré a la banda');
-        // console.log(body);
-        // Hacemos legible nuestro JSON
+        // Hacemos legible nuestro JSON para JS
+        // Un string a objeto
         const respuesta = JSON.parse(body);
-        const banda = new Banda(
-          respuesta.artists[0].strArtist,
-          respuesta.artists[0].intFormedYear,
-          respuesta.artists[0].strWebsite,
-          respuesta.artists[0].strGenre
-        )
+        // Instancia de mi clase Banda
+
+        // Creo constantes basadas en los atributos del objeto => respuesta.artists[0]
+        const { strArtist, intFormedYear, strWebsite, strGenre } = respuesta.artists[0];
+
+        // Instancia de mi clase Banda que se encuentra en Banda.js
+        const banda = new Banda(strArtist, intFormedYear, strWebsite, strGenre);
+
+        // Resuelve la promesa
         resolve(banda);
       } else {
         reject('No encontré a la banda');
