249e545068ca7a0d580475b7db31da9ae2da05df
Gerardo Medina <gerardo@devf.mx>
2020-06-03 22:45:51 -0500

    add CRUD_7

---
+++ 7_CRUD/main.js
@@ -0,0 +1,11 @@
+// CRUD (DB)
+// Create => Crear
+// Read => Leer
+// Update => Actualizar
+// Delete => Eliminar
+
+const request = require('request');
+
+request.post('Mi endpoint', miObjeto, () => {
+
+})
