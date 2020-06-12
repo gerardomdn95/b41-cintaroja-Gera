20a414e73234764c4081d9deb15c16ea9b04a9ca
Gera Medina <gerardo.mdn95@gmail.com>
2020-06-12 12:45:42 -0500

    fix styles

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,7 +1,6 @@
 // Dentro de APP vamos a llamar a nuestros containers
 import React from 'react';
 import TaskContainer from './containers/TaskContainer';
-import './App.css';
 
 const App = () => {
   return (
